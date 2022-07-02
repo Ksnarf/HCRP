c99d1f631da72c43d75bec92e7f0136ac6d0fd6f
Ksnarf <44852455+Ksnarf@users.noreply.github.com>
2022-07-01 17:31:00 -0700

    2021sil

---
+++ [veh-pd]/2021sil/__resource.lua
@@ -0,0 +1,14 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+-- Template made by McGinlay
+-- Please consider subscribing!
+
+files {
+	'vehicles.meta',
+	'carcols.meta',
+	'carvariations.meta'
+	
+}
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
