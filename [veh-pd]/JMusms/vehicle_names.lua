c74673a54336527f206631600c93243910666e4f
Ksnarf <44852455+Ksnarf@users.noreply.github.com>
2022-07-05 15:49:52 -0700

    rm marshall service vehs

--- [veh-pd]/JMusms/vehicle_names.lua
+++
@@ -1,8 +0,0 @@
-Citizen.CreateThread(function()
-	AddTextEntry('0x7BC86811', 'USMS Buffalo')
-	AddTextEntry('0x75B6973D', 'USMS Washington')
-	AddTextEntry('0x6161ACE3', 'USMS Van')
-	AddTextEntry('0x85878282', 'USMS Patriot')
-	AddTextEntry('0xE0EFDF4E', 'USMS Coach')
-	AddTextEntry('0xF38CFA80', 'USMS Merit')
-end)
