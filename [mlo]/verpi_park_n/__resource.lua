d983713ef72cba5c129de40e6b670346316a03a2
Ksnarf <44852455+Ksnarf@users.noreply.github.com>
2022-07-14 10:10:52 -0700

    MLO + verpi_park

---
+++ [mlo]/verpi_park_n/__resource.lua
@@ -0,0 +1,4 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+this_is_a_map 'yes'
+
+--This is a FREE Map made by me VerpiMoDz#2309 !! It was uploaded on 5Mods or Cfx.re! If you bought this from someone, make sure to DM me on Discord!--
