479cbac2457b35e506202b42f742e9f6916eb2f3
Ksnarf <44852455+Ksnarf@users.noreply.github.com>
2022-07-14 10:26:37 -0700

    rm all MLO's
    resetting to fix a resource load issue

--- [mlo]/medicgarage/__resource.lua
+++
@@ -1,5 +0,0 @@
-client_script {
-  "gui.lua",
-  "cl_pogarage.lua"
-}
-server_script "sv_pogarage.lua"
--- [mlo]/park/__resource.lua
+++
@@ -1,6 +0,0 @@
-data_file 'GTXD_PARENTING_DATA' 'meta/gtxd.meta' 
-file 'meta/gtxd.meta'
-
-this_is_a_map 'yes'
-client_script 'client.lua'
-data_file 'DLC_ITYP_REQUEST' 'park.ytyp'
