b3af779c5216c965bcdc2c8b5ccc1283929580ea
Ksnarf <44852455+Ksnarf@users.noreply.github.com>
2022-07-18 10:59:04 -0700

    veh-pd\pdvoltic ++
    because why the hell not

---
+++ [veh-pd]/pdvoltic/__resource.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+files {
+	'carcols.meta',
+	'carvariations.meta',
+	'dlctext.meta',
+	'handling.meta',
+	'vehicles.meta',
+	'*.meta'
+}
+data_file 'DLCTEXT_FILE' 'dlctext.meta'
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
