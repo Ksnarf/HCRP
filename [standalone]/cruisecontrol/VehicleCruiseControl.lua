d8278fc8f8f2875ebd6522fc05a7a79e6adb3cc9
Ksnarf <44852455+Ksnarf@users.noreply.github.com>
2022-07-27 22:25:35 -0700

    veh-lsfd\lsfdbrush ++

--- [standalone]/cruisecontrol/VehicleCruiseControl.lua
+++ [standalone]/cruisecontrol/VehicleCruiseControl.lua
@@ -1,15 +1,15 @@
 local ToggleKey
 
-JM36.CreateThread(function()
-	
+QBCore.CreateThread(function()
+
 	local SetControlNormal, math_min, math_abs, GetEntitySpeedVector, IsControlJustPressed
 		= SetControlNormal, math.min, math.abs, GetEntitySpeedVector, IsControlJustPressed
-	
+
 	local VehicleEligible, CruiseControlSpeed, LastVehicle
 	
 	local Player = Info.Player
 	local Vehicle = Player.Vehicle
-	
+
 	local yield = JM36.yield
 	while true do
 		if Vehicle.Id ~= LastVehicle and Vehicle.IsIn and Vehicle.IsOp then
@@ -18,7 +18,7 @@ JM36.CreateThread(function()
 			local Vehicle_Type = Vehicle.Type
 			VehicleEligible = Vehicle_Type.Bike or Vehicle_Type.Car or Vehicle_Type.Quadbike
 		end
-		
+
 		if VehicleEligible and Vehicle.IsIn and Vehicle.IsOp then
 			if IsControlJustPressed(0, ToggleKey) then
 				CruiseControlSpeed = not CruiseControlSpeed and GetEntitySpeedVector(LastVehicle, true).y
