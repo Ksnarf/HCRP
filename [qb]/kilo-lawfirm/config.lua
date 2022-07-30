21e041bec754d77be37479888269b65269c18f46
Ksnarf <44852455+Ksnarf@users.noreply.github.com>
2022-07-29 23:36:39 -0700

    Create config.lua

---
+++ [qb]/kilo-lawfirm/config.lua
@@ -0,0 +1,20 @@
+Config = {}
+
+Config.Objects = {
+  ["cone"] = {model='prop_roadcone02a'}, freeze = true},
+}
+
+Config.HandCuffItem = 'handcuffs'
+
+Config.LicenseRank = 1
+
+Config.UseTarget = GetConvar('UseTarget', 'false') == 'true'
+Config.Location = {
+  ["ClockIn"] = {  --
+    [1] =
+  },
+  ["vehicle"] = {
+    [1] =
+    [2] =
+  }
+}
